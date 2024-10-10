b99fe18e1bc802e10c55508432dafb37bfde6493
kshew2 <kshew2@uncc.edu>
2024-10-10 12:36:07 -0400

    Added

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+	container:{
+		center: true,
+		padding: "15px",
+		
+	},
+	screens: {
+		sm: '640px',
+		md: '768px',
+		lg: '960px',
+		xl: '1200px',
+	},
+	fontFamily: {
+		primary: "var(--font-jetbrainsMono)",
+	},
+  	extend: {
+  		colors: {
+  			primary: "#1c1c22",
+			accent: {
+				DEFAULT: '#00ff99',
+				hover: '#00e187',
+			},
+  		},
+  		// borderRadius: {
+  		// 	lg: 'border-radius: 0.5rem;',
+  		// 	md: 'border-radius: 0.375rem;',
+  		// 	sm: 'border-radius: 0.125rem;'
+  		// }
+  	}
+
+  },
+  plugins: [require("tailwindcss-animate")],
+};
